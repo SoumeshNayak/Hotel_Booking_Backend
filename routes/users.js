ff2c6dd7323ccb5f387703bcfab9fd12c0ee5d3d
Soumesh Nayak <soumeshnayak2210@gmail.com>
2024-10-02 01:09:24 +0530

    verified user from jwt token

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { deleteUser, getAllUser, getOneUser, updateUser } from '../controllers/user.js';
-import { verifyToken } from '../utils/verify.js';
+import { verifyToken, verifyUser } from '../utils/verify.js';
 
 
 
@@ -8,6 +8,9 @@ const router=express.Router();
 
 router.get("/checkauth",verifyToken,(req,res,next)=>{
     res.send("Hello user u r logged in")
+})
+router.get("/checkauth/:id",verifyUser,(req,res,next)=>{
+    res.send("Hello user,u r logged in and you can delete your account")
 })
 // Update
 router.put('/:id',updateUser)
