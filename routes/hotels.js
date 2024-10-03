996797b9c32b990a24ab6c0d647589120eb8add0
Soumesh Nayak <soumeshnayak2210@gmail.com>
2024-10-03 22:35:58 +0530

    added count functionality

--- routes/hotels.js
+++ routes/hotels.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose';
 import Hotel from '../model/Hotel.js';
-import { createHotel, deleteHotel, getAllHotel, getOneHotel, updateHotel } from '../controllers/hotel.js';
+import { countByCity, countByType, createHotel, deleteHotel, getAllHotel, getOneHotel, updateHotel } from '../controllers/hotel.js';
 import { verifyAdmin } from '../utils/verify.js';
 
 const router=express.Router();
@@ -12,9 +12,12 @@ router.put('/:id',verifyAdmin,updateHotel)
 //Delete
 router.delete('/:id',verifyAdmin,deleteHotel)
 // get
-router.get('/:id',getOneHotel)
+router.get('/find/:id',getOneHotel)
 //get all
 router.get('/',getAllHotel)
+router.get('/countByCity',countByCity)
+router.get('/countByType',countByType)
+
 
 
 export default router
