2861298fca19cea24003595327954f549a6c0c7c
Soumesh Nayak <soumeshnayak2210@gmail.com>
2024-10-03 23:14:28 +0530

    added cors

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ import usersRoute from './routes/users.js'
 import roomsRoute from './routes/rooms.js'
 import hotelsRoute from './routes/hotels.js'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
+
 dotenv.config()
 const connect= async () =>{
     try {
@@ -28,7 +30,7 @@ mongoose.connection.on("connected",()=>{
 //Middlewares
 app.use(cookieParser())
 app.use(express.json())
-
+app.use(cors())
 
 
 app.use('/auth',authRoute)
